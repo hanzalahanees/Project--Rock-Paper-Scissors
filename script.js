d7adbc1d4c09fe9547bcd7f56ca9a7565d2762c3
Fazal Ur Rehman <anees_pk4@yahoo.com>
2023-01-30 08:12:14 +0500

    Function playRound created

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function getComputerChoice() {
     let randomRPS = choice[Math.floor(Math.random() * choice.length)];
     return randomRPS;
 }
+
+// play a single round of the game
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Rock beats Scissors";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win! Scissors beats Paper";
+  } else if (playerSelection == computerSelection) {
+    return "Draw";
+  }
+}
